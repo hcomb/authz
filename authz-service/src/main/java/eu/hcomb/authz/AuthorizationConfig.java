a80cec78d3366b73ca5476470b961e52f46e201d
Alessandro Siletto <alessandro.siletto@gmail.com>
2015-12-08 17:40:43 +0100

    swagger setup (not enabled)

--- authz-service/src/main/java/eu/hcomb/authz/AuthorizationConfig.java
+++ authz-service/src/main/java/eu/hcomb/authz/AuthorizationConfig.java
@@ -4,17 +4,27 @@ import eu.hcomb.common.cors.CorsConfig;
 import eu.hcomb.common.cors.CorsConfigurable;
 import eu.hcomb.common.jdbc.JdbcConfig;
 import eu.hcomb.common.jdbc.JdbcConfigurable;
+import eu.hcomb.common.swagger.SwaggerConfig;
+import eu.hcomb.common.swagger.SwaggerConfigurable;
 import eu.hcomb.common.web.BaseConfig;
 
-public class AuthorizationConfig extends BaseConfig implements CorsConfigurable, JdbcConfigurable {
+public class AuthorizationConfig extends BaseConfig implements CorsConfigurable, JdbcConfigurable, SwaggerConfigurable {
 
 	protected JdbcConfig jdbcConfig = new JdbcConfig();
+	
+	protected CorsConfig corsConfig = new CorsConfig();
+
+	protected SwaggerConfig swaggerConfig = new SwaggerConfig();
+
+	public SwaggerConfig getSwaggerConfig() {
+		return swaggerConfig;
+	}
+
 
 	public JdbcConfig getJdbcConfig() {
 		return jdbcConfig;
 	}
 	
-	protected CorsConfig corsConfig = new CorsConfig();
 
 	public CorsConfig getCorsConfig() {
 		return corsConfig;
